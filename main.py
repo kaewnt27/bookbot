6de4c46774ffb21245260fddb4d92b76ca1b7e94
kaewnt <kaewnt@gmail.com>
2025-10-15 13:48:46 +0700

    Simplify Code

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
-from stats import get_num_words
+from stats import get_num_words, get_char_dict
+
+def get_book_text(path):
+	path = "books/frankenstein.txt"
+	with open(path) as f:
+		return f.read()
 
 def main():
-	print(f"Found {get_num_words()} total words")
+	book_path = "books.frankenstein.txt"
+	text = get_book_text(book_path)
+	num_words = get_num_words(text)
+	chars_dict = get_char_dict(text)
+	print(f"Found {num_words} total words")
+	print(chars_dict)
 
 main()
--- stats.py
+++ stats.py
@@ -1,36 +1,13 @@
-def get_book_text():
-        with open("books/frankenstein.txt") as f:
-                file_contents = f.read()
-                return file_contents
+def get_num_words(text):
+	words = text.split()
+	return  len(words)
 
-def get_num_words():
-        file_contents = get_book_text()
-        words = file_contents.split()
-        num_words = len(words)
-        return num_words
-
-def character_counts():
-	file_contents = get_book_text()
-	lower_text = file_contents.lower()
-	char_list = list(lower_text)
-	unique_char = set(lower_text)
-	unique_char_count = []
-
-	for each_char in unique_char:
-		char_count = 0
-		for char in char_list:
-			if(each_char == char):
-				char_count +=1
-		unique_char_count.append(char_count)
-	
-	unique_char_list = list(unique_char)
-	combine_set = {}
-	for i in range (0, len(unique_char_list)):
-		combine_set[unique_char_list[i]] = unique_char_count[i]
-
-	print(combine_set)
-
-def main():
-	character_counts()
-
-main()
+def get_char_dict(text):
+	chars = {}
+	for c in text:
+		lowered = c.lower()
+		if lowered in chars:
+			chars[lowered] +=1
+		else:
+			chars[lowered] = 1
+	return chars
